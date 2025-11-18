c3343f753b462e2e7dd550f5a3f6b3dec97ec708
K-Nguyen-92 <knguyen7792@gmail.com>
2025-11-18 15:36:30 -0500

    Implement advanced middleware

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ const mainRouter = require("./routes/index");
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: "https://gcp-wtwr.ignorelist.com",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 // app.use((req, res, next) => {
 //   req.user = {
 //     _id: "68d04a07733f93c6cd4b16ad",
