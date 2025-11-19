b980e914de979780610af5c5c03b8f39faf5da1c
K-Nguyen-92 <knguyen7792@gmail.com>
2025-11-19 15:20:09 -0500

    set up server crash testing

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ app.use(
     credentials: true,
   })
 );
+app.use(requestLogger);
 app.get("/crash-test", () => {
   setTimeout(() => {
     throw new Error("Server will crash now");
   }, 0);
 });
 app.use("/", mainRouter);
-app.use(requestLogger);
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
